4acd1d561712507e8a8f00863b3437bb288c5b55
Valtteri Lakaniemi <valtteri.lakaniemi@aalto.fi>
2020-04-22 22:53:57 +0300

    Add test version of the map, try react-map-interaction for touch interaction

---
+++ src/Map.js
@@ -0,0 +1,220 @@
+import React from "react";
+
+export const Map = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fillRule="evenodd"
+    clipRule="evenodd"
+    viewBox="0 0 4096 2534"
+  >
+    <path id="bcg" fill="#fff" d="M0 0H4096V2534H0z"></path>
+    <path
+      id="childrenHealthCare"
+      fill="#E6A4FF"
+      d="M3460 716.09l.07 105 139-.09-.07-105-139 .09zm445 9.91h160v1148h-160V726zm-546 362h113v139h-113v-139zm234 0h-115v139h115v-139zm13 0h105v139h-105v-139zm-133 245h-113v139h113v-139zm6 0h115v139h-115v-139zm233 0h-105v139h105v-139zm14-245h112v139h-112v-139zm-265.93-145.91l-.07-105 139-.09.07 105-139 .09zm-.07 23l.07 105 139-.09-.07-105-139 .09z"
+    ></path>
+    <path
+      id="yesBox"
+      fill="#24BD66"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2983.5 1404.5H3154.5V1557.5H2983.5z"
+    ></path>
+    <path
+      id="info"
+      fill="#2F80ED"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2696.5 1404.5H2976.5V1557.5H2696.5z"
+    ></path>
+    <path
+      id="kidsLibraryInfo"
+      fill="#2F80ED"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M3381.5 1597.5H3512.5V1701.5H3381.5z"
+    ></path>
+    <path
+      id="kidsArea"
+      fill="#24BD66"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M3582.5 1531.5H3836.5V1767.5H3582.5z"
+    ></path>
+    <path
+      id="studio"
+      fill="#FF7E7E"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2496.5 2024.5H2693.5V2096.5H2496.5z"
+    ></path>
+    <path
+      id="musicRoom"
+      fill="#FF7E7E"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2496.5 2115.5H2693.5V2229.5H2496.5z"
+    ></path>
+    <path
+      id="stage"
+      fill="#FF7E7E"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2789.5 1948.5H3011.5V2241.5H2789.5z"
+    ></path>
+    <path
+      id="paja"
+      fill="#FF7E7E"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2951.5 2303.5H3701.5V2495.5H2951.5z"
+    ></path>
+    <path
+      id="ypp"
+      fill="#FFB574"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2072.5 1825.5H2415.5V1931.5H2072.5z"
+    ></path>
+    <path
+      id="meetingSpace"
+      fill="#FFB574"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2488.5 1825.5H2693.5V2005.5H2488.5z"
+    ></path>
+    <path
+      id="kela"
+      fill="#FFB574"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M1633.5 1825.5H2038.5V1931.5H1633.5z"
+    ></path>
+    <path
+      id="healthCareCenter"
+      fill="#E6A4FF"
+      d="M458 581l-97.5-33.5-30 95H352V749h106V581zm109 38.5L462 581v168h105V619.5zM151 846h109v144H151V846zm0 154h109v144H151v-144zm141 228H157v112h135v-112zm-135 119h135v139H157v-139zm278 0H300v139h135v-139zm-135-119h135v112H300v-112zm75-382H266v144h109V846zm-109 154h109v144H266v-144zm224-154H381v144h109V846zm-109 154h109v144H381v-144zm224-154H496v144h109V846zm-109 154h109v144H496v-144zM198.612 750.756l-.23-106.1 150.368-.326.231 106.099-150.369.327z"
+    ></path>
+    <g id="wc" fill="#BDBDBD" stroke="#4F4F4F" strokeWidth="3">
+      <path d="M3523.5 1875.5H3903.5V2021.5H3523.5z"></path>
+      <path d="M720.5 1426.5H1000.5V1671.5H720.5z"></path>
+      <path d="M1877.5 285.5H2192.5V408.5H1877.5z"></path>
+    </g>
+    <g id="meetinRooms">
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M2263.5 2303.5H2422.5V2495.5H2263.5z"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M1044.66 1788.22H1212.813V1956.278H1044.66z"
+        transform="rotate(-6.342 1044.667 1788.221)"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M2425.5 2303.5H2584.5V2495.5H2425.5z"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M2587.5 2303.5H2746.5V2495.5H2587.5z"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M2749.5 2303.5H2948.5V2495.5H2749.5z"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M3906.5 513.5H4063.5V705.5H3906.5z"
+      ></path>
+      <path
+        fill="#9AE0F6"
+        fillRule="nonzero"
+        stroke="#4F4F4F"
+        strokeWidth="3"
+        d="M578 772.879V626.633l195.575 70.269-44.002 120.76L578 772.879z"
+      ></path>
+    </g>
+    <path
+      id="quietArea"
+      fill="#FED863"
+      fillRule="nonzero"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M170 2099.5v163h378.5V2463h-514v-363.5H170z"
+    ></path>
+    <path
+      id="VOX"
+      fill="#24BD66"
+      fillRule="nonzero"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M3452 1079h-102.5v72.5H3201v-71.28l-.1-.27-103.46-262.63L3360.3 708h91.7v371z"
+    ></path>
+    <path
+      id="elevators"
+      fill="#CB731C"
+      d="M1883 499h60v85h-60v-85zm1675 0h-60v85h60v-85zm73 0h-60v85h60v-85zm13 0h60v85h-60v-85zm133 0h-60v85h60v-85zm-1761 0h-60v85h60v-85zm13 0h60v85h-60v-85zm133 0h-60v85h60v-85z"
+    ></path>
+    <path
+      id="outline"
+      fill="none"
+      fillRule="nonzero"
+      stroke="#000"
+      strokeWidth="8"
+      d="M1509 1582.5h-308.5v-47l109-662L22.5 419v2090.5h1287l-95-749H1509v749h2566V29.5H624.5V323l1688 847.5V1412L1509 1535.5v47z"
+    ></path>
+    <path
+      id="rtg"
+      fill="#E6A4FF"
+      fillRule="nonzero"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2272 142.5h607v647.015l-289.45 125.329L2272 754.577V142.5z"
+    ></path>
+    <path
+      id="dentalClinic"
+      fill="#E6A4FF"
+      fillRule="nonzero"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M2892.5 142.5h492v440.993l-492 201.273V142.5z"
+    ></path>
+    <path
+      id="mentalHealthCenter"
+      fill="#E6A4FF"
+      d="M1062.76 1958.29l169-17.17 8.98 88.44-169 17.17-8.98-88.44zm9.24 97.87l168.99-17.16 8.99 88.44-169 17.16-8.98-88.44zm181.99 81.84L1085 2155.16l8.98 88.44 169-17.16-8.99-88.44zM1096 2249.5l167-17 10.5 105.5h-166l-11.5-88.5zm175 93h-151.5v117l166.5 3.5-15-120.5z"
+    ></path>
+    <path
+      id="mentalHealthCenter1"
+      fill="#4F4F4F"
+      fillRule="nonzero"
+      d="M1231.76 1941.12l2.98-.3-.3-2.98-2.99.3.31 2.98zm-169 17.17l-.3-2.99-2.99.3.31 2.99 2.98-.3zm177.98 71.27l.3 2.99 2.98-.3-.3-2.99-2.98.3zm-169 17.17l-2.98.3.3 2.98 2.98-.3-.3-2.98zm169.25-7.73l2.99-.3-.3-2.99-2.99.31.3 2.98zM1072 2056.16l-.3-2.98-2.99.3.31 2.99 2.98-.31zm177.98 71.28l.3 2.98 2.98-.3-.3-2.98-2.98.3zm-169 17.16l-2.98.3.3 2.99 2.98-.3-.3-2.99zm4.02 10.56l-.3-2.98-2.99.3.31 2.99 2.98-.31zm168.99-17.16l2.99-.3-.3-2.99-2.99.31.3 2.98zm-160.01 105.6l-2.98.3.3 2.99 2.98-.3-.3-2.99zm169-17.16l.3 2.98 2.98-.3-.3-2.98-2.98.3zm.02 6.06l2.99-.3-.3-2.99-2.99.3.3 2.99zm-167 17l-.3-2.98-3.07.31.4 3.06 2.97-.39zm177.5 88.5v3h3.31l-.32-3.3-2.99.3zm-166 0l-2.98.39.34 2.61h2.64v-3zm12 4.5v-3h-3v3h3zm151.5 0l2.98-.37-.33-2.63H1271v3zm-151.5 117h-3v2.94l2.94.06.06-3zm166.5 3.5l-.06 3 3.47.07-.43-3.44-2.98.37zm-54.55-524.86l-168.99 17.16.6 5.97 169-17.16-.61-5.97zm12.27 91.12l-8.98-88.44-5.97.61 8.98 88.44 5.97-.61zm-171.68 20.45l169-17.16-.61-5.97-168.99 17.16.6 5.97zm-12.26-91.12l8.98 88.44 5.97-.61-8.99-88.44-5.96.61zm180.91 77.43l-168.99 17.16.6 5.97 169-17.17-.61-5.96zm12.27 91.12l-8.98-88.44-5.97.6 8.98 88.44 5.97-.6zm-171.68 20.45l169-17.17-.61-5.97-168.99 17.17.6 5.97zm-12.26-91.12l8.98 88.43 5.97-.6-8.99-88.44-5.96.61zm16.28 101.68l169-17.17-.61-5.96-168.99 17.16.6 5.97zm11.67 85.15l-8.99-88.44-5.96.61 8.98 88.43 5.97-.6zm165.7-19.85l-168.99 17.17.6 5.97 169-17.17-.61-5.97zm-11.66-85.15l8.98 88.44 5.97-.6-8.98-88.44-5.97.6zm11.69 91.21l-167 17.01.6 5.96 167-17-.6-5.97zm13.79 108.19l-10.5-105.5-5.98.6 10.5 105.5 5.98-.6zm-168.99 3.3h166v-6h-166v6zm-14.47-91.11l11.49 88.5 5.95-.78-11.49-88.5-5.95.78zm26.47 95.61H1271v-6h-151.5v6zm3 114v-117h-6v117h6zm163.56.5l-166.5-3.5-.12 6 166.5 3.5.12-6zm-18.04-117.13l15 120.5 5.96-.74-15-120.5-5.96.74z"
+    ></path>
+    <path
+      id="childrenBooks"
+      fill="#2D9CDB"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M3477.5 1861c0-40.01-32.49-72.5-72.5-72.5h-258c-40.01 0-72.5 32.49-72.5 72.5v267c0 40.01 32.49 72.5 72.5 72.5h258c40.01 0 72.5-32.49 72.5-72.5v-267z"
+    ></path>
+    <path
+      id="music"
+      fill="#2D9CDB"
+      stroke="#4F4F4F"
+      strokeWidth="3"
+      d="M3291.5 1629c0-17.38-14.11-31.5-31.5-31.5h-377c-17.38 0-31.5 14.12-31.5 31.5v88c0 17.38 14.12 31.5 31.5 31.5h377c17.39 0 31.5-14.12 31.5-31.5v-88z"
+    ></path>
+  </svg>
+);
